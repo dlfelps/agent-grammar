805a161b7793490978e10c077b8b46b2564fef7c
dlfelps <dlfelps@gmail.com>
2026-06-04 17:47:15 -0400

    Bump version from 0.1.2 to 0.1.3

--- src/agent_grammar/__init__.py
+++ src/agent_grammar/__init__.py
@@ -2,6 +2,6 @@
 
 from agent_grammar.testing import AgentTestClient, step_boundary, workflow
 
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 
 __all__ = ["AgentTestClient", "step_boundary", "workflow", "__version__"]
